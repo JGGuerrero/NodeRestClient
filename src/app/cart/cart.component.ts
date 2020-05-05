8238537b7a1fa6c31cfc525446e609a3edbd48e6
JGGuerrero <jgguerrero@mail.fresnostate.edu>
2020-05-05 15:27:19 -0700

    Functionality for Clothing Store

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -22,6 +22,7 @@ export class CartComponent implements OnInit {
   bikes: Array<IBike> = [];
   myName = '';
   cars = [];
+  clothings = [];
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
@@ -30,12 +31,27 @@ export class CartComponent implements OnInit {
   ) { }
 
   async ngOnInit() {
-    await this.refresh();
+    // await this.refresh();
     // this.createCar('car', {make: 'Tesla', model: 'X'});
     // this.updateCar('car/id/1', {make: 'Ford', model: 'Fiesta'});
   }
 
+  //THIS IS TO REFRESH CARS ON INITIALIZATION
+  // async refresh() {
+  //   this.cars = await this.getCars('car');
+  // }
+
+  //gets clothing
   async refresh() {
+    this.clothings = await this.getClothings('clothing');
+  }
+
+  async getClothingByColor() {
+    console.log('from pink')
+    this.clothings = await this.getClothings('Clothing/get-clothing-by-color/Pink');
+  }
+
+  async showCars() {
     this.cars = await this.getCars('car');
   }
 
@@ -43,6 +59,12 @@ export class CartComponent implements OnInit {
   async getCars(path: string) {
     const resp = await this.http.get(path);
     console.log('resp from getCars(), resp')
+    return resp;
+  }
+
+  async getClothings(path: string) {
+    const resp = await this.http.get(path);
+    console.log('resp from getClothing(), resp')
     return resp;
   }
 
@@ -63,11 +85,37 @@ export class CartComponent implements OnInit {
     return resp;
   }
 
+  async createClothing() {
+    const clothing = {
+      image_url: null,
+      clothing_type: null,
+      color: null
+    };
+    const resp = await this.http.post('clothing', clothing);
+    console.log('from createClothing resp: ', resp);
+    if(resp){
+      // this.refresh();
+      this.clothings.unshift(resp);
+    } else {
+      this.toastService.showToast('Danger', 3000, 'Car creation failed!');
+    }
+    return resp;
+  }
+
   async updateCar(car: any) {
     console.log('from updateCar car: ', car);
     const resp = await this.http.put(`car/id/${car.id}`, car);
     if (resp) {
       this.toastService.showToast('success', 3000, 'Car updated successfully!');
+    }
+    return resp;
+  }
+
+  async updateClothing(clothing: any) {
+    console.log('from updateClothing Clothing: ', clothing);
+    const resp = await this.http.put(`clothing/id/${clothing.id}`, clothing);
+    if (resp) {
+      this.toastService.showToast('success', 3000, 'Clothing updated successfully!');
     }
     return resp;
   }
@@ -81,6 +129,18 @@ export class CartComponent implements OnInit {
       this.refresh();
     } else {
       this.toastService.showToast('danger', 3000, 'Delete car failed!');
+    }
+  }
+
+  async removeClothing(clothing: any, index: number) {
+    console.log('from removeClothing...', index);
+    // this.cars.splice(index, 1);
+    const resp = await this.http.delete(`clothing/id/${clothing.id}`);
+    console.log('resp from removeClothing...', resp);
+    if (resp) {
+      this.refresh();
+    } else {
+      this.toastService.showToast('danger', 3000, 'Delete clothing failed!');
     }
 
   }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -25,14 +25,14 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
-    const token = localStorage.getItem('id_token');
-    console.log('from login ngOnInit toekn: ', token);
-    if(token != null) {
-      this.loggedIn = true;
-      this.router.navigate(['']);
-    } else {
-      this.loggedIn = false;
-    }
+    // const token = localStorage.getItem('id_token');
+    // console.log('from login ngOnInit toekn: ', token);
+    // if(token != null) {
+    //   this.loggedIn = true;
+    //   this.router.navigate(['']);
+    // } else {
+    //   this.loggedIn = false;
+    // }
   }
 
 
